2e9e857e5692eae05beb50ba6524d1d1515054f0
CubeSky <max_301@live.com>
2018-06-16 22:39:30 +0800

    Avoid Code Warning

--- skyrecycleradapter/src/main/java/liyin/party/skyrecycleradapter/EmptyDataBean.java
+++ skyrecycleradapter/src/main/java/liyin/party/skyrecycleradapter/EmptyDataBean.java
@@ -4,13 +4,11 @@ import android.util.SparseArray;
 import android.util.SparseIntArray;
 
 public class EmptyDataBean extends AutoDataBean{
-    protected int layout_id;
     private static int layout_next = -1;
     protected int layout_type;
     protected static SparseIntArray empty_layout = new SparseIntArray(); //<Type,Layout>
-    private static SparseArray<EmptyDataBean> empty_layout_reverse = new SparseArray<EmptyDataBean>(); //<Layout,Type>
-    private EmptyDataBean(int layout_id, int layout_type) {
-        this.layout_id = layout_id;
+    private static SparseArray<EmptyDataBean> empty_layout_reverse = new SparseArray<>(); //<Layout,Type>
+    private EmptyDataBean(int layout_type) {
         this.layout_type = layout_type;
     }
     public static EmptyDataBean getEmptyLayout(int layout_id) {
@@ -18,7 +16,7 @@ public class EmptyDataBean extends AutoDataBean{
         if (hasEmptyDataBean != null) {
             return hasEmptyDataBean;
         }
-        EmptyDataBean emptyDataBean = new EmptyDataBean(layout_id, layout_next);
+        EmptyDataBean emptyDataBean = new EmptyDataBean(layout_next);
         empty_layout.append(layout_next, layout_id);
         empty_layout_reverse.append(layout_id, emptyDataBean);
         EmptyDataBean.layout_next = EmptyDataBean.layout_next - 1;
